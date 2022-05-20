893a6fe1f8fce4286fe0b5844a2a6c4963f41836
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-05-20 11:25:26 +0500

    configured nlog, and handle_order method implemented

--- Program.cs
+++ Program.cs
@@ -1,25 +1,67 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace graceful_shutdown_asp_app
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLogBuilder
+                .ConfigureNLog("nlog.config")
+                .GetCurrentClassLogger();
+
+            try  
+            {  
+                logger.Info("Application starting...");  
+                CreateHostBuilder(args).Build().Run();
+            }  
+            catch (Exception ex)  
+            {  
+                logger.Error(ex, "Fatal error was expected!");  
+                throw;
+            }  
+            finally  
+            {  
+                NLog.LogManager.Shutdown();  
+            }  
+        }
+
+        private static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>  
+                {  
+                    logging.ClearProviders();  
+                    logging.SetMinimumLevel(LogLevel.Information);  
+                })
+                .UseNLog()
+                .Configure(app =>
+                {
+                    // An example ASP.NET Core middleware that throws an
+                    // exception when serving a request to path: /throw
+                    app.UseRouting();
+                    
+                    app.UseEndpoints(endpoints =>
+                    {
+                        // Reported events will be grouped by route pattern
+                        endpoints.MapGet("/handle_order", context =>
+                        {
+                            var log = context.RequestServices
+                                .GetRequiredService<ILoggerFactory>()
+                                .CreateLogger<Program>();
+
+                            log.LogInformation("Handling order request...");
+                            
+                            return Task.CompletedTask;
+                        });
+                    });
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
