72efa0b76c561efd7ca185820a36cde1d425b5e3
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-05-20 17:54:06 +0500

    implemented host service

--- Hosts/HostApplicationLifeTime.cs
+++ Hosts/HostApplicationLifeTime.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
+using graceful_shutdown_asp_app.Services;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace graceful_shutdown_asp_app.Hosts
 {
     public class HostApplicationLifeTime : IHostedService
     {
         private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly ServerStateService _serverStateService;
+        private readonly ILogger<HostApplicationLifeTime> _logger;
 
         public HostApplicationLifeTime(
-            IHostApplicationLifetime hostApplicationLifetime)
-            => _hostApplicationLifetime = hostApplicationLifetime;
-        
+            IHostApplicationLifetime hostApplicationLifetime,
+            ILoggerFactory loggerFactory,
+            ServerStateService serverStateService)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+            _serverStateService = serverStateService;
+            _logger = loggerFactory.CreateLogger<HostApplicationLifeTime>();
+        }
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _hostApplicationLifetime.ApplicationStarted.Register(OnStarted);
@@ -27,17 +36,21 @@ namespace graceful_shutdown_asp_app.Hosts
         
         private void OnStarted()
         {
-            Console.WriteLine("Started...");
+            _serverStateService.ApplicationStarted();
+            _logger.LogInformation($"Application State: {_serverStateService.ApplicationState}");
         }
 
         private void OnStopping()
         {
-            Console.WriteLine("Stopping...");
+            _serverStateService.ApplicationStopping();
+            Task.Delay(12000).Wait();
+            _logger.LogInformation($"Application State: {_serverStateService.ApplicationState}");
         }
 
         private void OnStopped()
         {
-            Console.WriteLine("Stopped...");
+            _serverStateService.ApplicationStopped();
+            _logger.LogInformation($"Application State: {_serverStateService.ApplicationState}");
         }
     }
 }
